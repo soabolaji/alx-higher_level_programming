db21212420163c460756ba945bda3b31bce9e905
soabolaji <soabolaji@gmail.com>
2023-01-03 07:55:03 -0800

    positive fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,9 @@ for i in range(10):
   for j in range(10):
     if i != j:
       print(f"{i:01d}{j:01d}", end=", " if not (i == 9 and j == 8) else "\n")
-
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 def islower(c):
+    """Check for lowercase characters."""
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
