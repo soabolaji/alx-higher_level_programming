b34a3f31d9dc0686969cf6ada65571d3f0de9a4d
soabolaji <soabolaji@gmail.com>
2023-01-03 08:33:32 -0800

    positive

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-  for j in range(10):
-    if i != j:
-      print(f"{i:01d}{j:01d}", end=", " if not (i == 9 and j == 8) else "\n")
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, j))
         else:
-            print("{}{}".format(digit1, digit2), end=", ")
+            print("{}{}".format(i, j), end=", ")
