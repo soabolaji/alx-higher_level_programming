8527e0b326a97f74bab63c06aefd07ad332e5100
soabolaji <soabolaji@gmail.com>
2023-01-05 01:37:29 -0800

    maths

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+def add(a, b):
+    """ Addition function
+
+        Agrs:
+
+        a: first integer
+        b: second integer
+
+        the return value is a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """ Subtraction function
+
+        Agrs:
+
+        a: first integer
+        b: second integer
+                                                 the return value is a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """ Multiplication function
+
+        Agrs:
+
+        a: first integer
+        b: second integer
+                                                 the return value is a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """ Division function
+
+        Agrs:
+
+        a: first integer
+        b: second integer
+                                                 the return value is a / b
+    """
+    return (a / b)
