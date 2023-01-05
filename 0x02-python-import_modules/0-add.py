32c465b9c35497b0362f4ae78853f60a44c2b5bc
soabolaji <soabolaji@gmail.com>
2023-01-05 00:47:59 -0800

    addition

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#add_0.py
+
+if __name__ = "__main__":
+    """ print the value of 1 and 2 """
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add(a, b):
+    """ Addition function
+
+
+        Args:
+            
+            a: first integer
+            b: second integer
+
+            return:
+                the value is a + b
+        """
+        return (a + b)
