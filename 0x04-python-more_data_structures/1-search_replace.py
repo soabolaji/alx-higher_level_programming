4cf8ce817b623c65b4773492c800b4e5ea2f04bd
root <root@LAPTOP-ND7MH9ET>
2023-01-11 13:51:00 +0100

    square matrix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 
+
 def search_replace(my_list, search, replace):
-    """Replace all occurrences of an element by another in a new list."""
-    new_list = my_list[:]
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-            return (new_list)
+    new_list = my_list.copy()
+    idx_list = [i for i, el in enumerate(my_list) if el == search]
+    for idx in idx_list:
+        new_list[idx] = replace
+        return new_list
