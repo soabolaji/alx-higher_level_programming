138e297ecbc1087b53ef214c5bd402f67b465af6
soabolaji <soabolaji@gmail.com>
2023-01-11 12:50:57 -0800

    0_squares

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-
-
 def square_matrix_simple(matrix=[]):
-    lenM = len(matrix)
-    new_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(lenM)]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            new_matrix[i][j] = matrix[i][j]*matrix[i][j]
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
     return new_matrix
