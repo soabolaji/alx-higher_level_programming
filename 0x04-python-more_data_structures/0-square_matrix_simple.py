6e762d6ac3e3ef87b3757a24c7fe1215dd4955d7
soabolaji <soabolaji@gmail.com>
2023-01-11 11:39:30 -0800

    square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-
-
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+    if not matrix:
+        print()
+    return [[item**2 for item in row] for row in matrix]
