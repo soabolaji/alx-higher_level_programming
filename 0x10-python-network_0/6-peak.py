81aea3e91c8503000ed24e70e1344d70a9149c02
soabolaji <soabolaji@gmail.com>
2023-04-27 16:54:16 +0100

    Python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+"""Defines a peak-finding algorithm."""
+
 
 def find_peak(list_of_integers):
-    """
-    Finds a peak in a list of unsorted integers.
-    """
-    n = len(list_of_integers)
-    if n == 0:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
-    if n == 1:
-        return list_of_integers[0]
-    if n == 2:
-        return max(list_of_integers)
-    mid = n // 2
-    if list_of_integers[mid] < list_of_integers[mid-1]:
-        return find_peak(list_of_integers[:mid])
-    elif list_of_integers[mid] < list_of_integers[mid+1]:
-        return find_peak(list_of_integers[mid+1:])
-    else:
-        return list_of_integers[mid]
 
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
